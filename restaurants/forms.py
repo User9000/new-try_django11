b8feab7981f7a696df863e1e755349cf84070d66
Carlo Lam <calam12@gmail.com>
2018-03-26 16:10:43 -0700

    validators
    validators for category

--- restaurants/forms.py
+++ restaurants/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import RestaurantLocation
-
+from .validators import validate_category
 
 class RestaurantCreateForm(forms.Form):
     name        =    forms.CharField()
@@ -16,6 +16,7 @@ class RestaurantCreateForm(forms.Form):
 
 
 class RestaurantLocationCreateForm(forms.ModelForm):
+    category = forms.CharField(required = False,validators= [validate_category])
     class Meta:
             model = RestaurantLocation
             fields = [
@@ -23,3 +24,15 @@ class RestaurantLocationCreateForm(forms.ModelForm):
                 'location',
                 'category'
             ]
+    def clean_name(self):
+        name = self.cleaned_data.get("name")
+        if name == 'Hello':
+            raise forms.ValidationError("Error")
+        return name
+    
+    def clean_email(self):
+        name = self.cleaned_data.get("name")
+        if name == 'Hello':
+            raise forms.ValidationError("Error")
+        return name
+
---
+++ restaurants/validators.py
@@ -0,0 +1,21 @@
+from django.core.exceptions import ValidationError
+
+
+def validate_even(value):
+    if value % 2 != 0:
+        raise ValidationError(
+            '%(value)s is not an even number',
+            params={'value': value},
+        )
+
+def validate_email(value):
+    email  = value
+    if 'edu' in value:
+       raise ValidationError("We do not accept edu emails")
+
+
+CATEGORIES = ['Mexican', 'Asian', 'American', 'Whatever']
+
+def validate_category(value):
+    if not value in CATEGORIES:
+        raise ValidationError(f'This {value} is not a valid category')

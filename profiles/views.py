70cf4c39eb8b45764a438935c24bdc68df98c7d5
Carlo Lam <calam12@gmail.com>
2018-04-08 11:26:05 -0700

    addition of profiles user.html
    addition of user.html to profiles app

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
-from django.crontrib.auth import get_user_model
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 
 # Create your views here.
@@ -7,5 +8,13 @@ from django.views.generic import DetailView
 User = get_user_model()
 
 class ProfileDetailView(DetailView):
-    queryset = User.objects.fiter(is_acitve=True)
+    queryset = User.objects.filter(is_active=True)
+    template_name = 'profiles/user.html'
+
+
+    def get_object(self):
+        username = self.kwargs.get("username")
+        if username is None:
+            raise Http404
+        return get_object_or_404(User, username__iexact=username, is_active = True)
 

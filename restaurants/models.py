f3884dfdf0ecf865f8c69c60822ce2460d9c3c30
Carlo Lam <calam12@gmail.com>
2018-04-08 17:43:48 -0700

    added search feature using Q
    added search feature using Q

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,15 +1,40 @@
 from django.db import models
-
+from django.db.models import Q
 from django.conf import settings
 from django.db.models.signals import pre_save, post_save
 from .utils import unique_slug_generator
 from .validators import validate_category
-
 from django.core.urlresolvers import reverse
 # Create your models here.
 
 
 User = settings.AUTH_USER_MODEL
+
+class RestaurantLocationQuerySet(models.query.QuerySet):
+    def search(self,query):
+        if query:
+                return self.filter(
+            Q(name__icontains=query)     | 
+            Q(location__icontains=query) |
+            Q(category__icontains=query) |
+            Q(location__icontains=query) |
+            Q(item__name__icontains=query)|
+            Q(item__contents__icontains=query) ).distinct()
+        return self
+
+
+
+
+#Model Manager
+class RestaurantLocationManager(models.Manager):
+
+    def get_queryset(self):
+        return RestaurantLocationQuerySet(self.model, using= self._db)
+
+
+    def search(self,query):
+        return self.get_queryset().search(query)
+
 
 class RestaurantLocation(models.Model):
 
@@ -22,7 +47,7 @@ class RestaurantLocation(models.Model):
     updated     =    models.DateTimeField(auto_now=True)
     slug        =    models.SlugField(null=True, blank=True)
 
-
+    objects = RestaurantLocationManager()
 
     def __str__(self):
         return self.name
